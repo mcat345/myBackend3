77e7b740a473993ce4531f99b20e71ae7d21ba08
Denis Baraley <dbaraley@netpulse.com>
2019-01-31 16:01:15 +0200

    add chapter 10

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -33,51 +33,49 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
 public func configure(
-    _ config: inout Config,
-    _ env: inout Environment,
-    _ services: inout Services
-    ) throws {
-    // Register providers first
-    try services.register(FluentPostgreSQLProvider())
+  _ config: inout Config,
+  _ env: inout Environment,
+  _ services: inout Services
+) throws {
+  // Register providers first
+  try services.register(FluentPostgreSQLProvider())
 
-    // Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
+  // Register routes to the router
+  let router = EngineRouter.default()
+  try routes(router)
+  services.register(router, as: Router.self)
 
-    // Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    services.register(middlewares)
+  // Register middleware
+  var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+  // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+  middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+  services.register(middlewares)
 
-    // Configure a database
-    var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+  // Configure a database
+  var databases = DatabasesConfig()
+  let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+  let username = Environment.get("DATABASE_USER") ?? "vapor"
+  let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+  let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+  let databaseConfig = PostgreSQLDatabaseConfig(
+    hostname: hostname,
+    username: username,
+    database: databaseName,
+    password: password)
+  let database = PostgreSQLDatabase(config: databaseConfig)
+  databases.add(database: database, as: .psql)
+  services.register(databases)
 
-    let databaseConfig: PostgreSQLDatabaseConfig
+  // Configure migrations
+  var migrations = MigrationConfig()
+  migrations.add(model: User.self, database: .psql)
+  migrations.add(model: Acronym.self, database: .psql)
+  migrations.add(model: Category.self, database: .psql)
+  migrations.add(model: AcronymCategoryPivot.self, database: .psql)
+  services.register(migrations)
 
-    if let url = Environment.get("DATABASE_URL") {
-        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
-    } else {
-        databaseConfig = PostgreSQLDatabaseConfig(
-            hostname: hostname,
-            username: username,
-            database: databaseName,
-            password: password)
-    }
-
-    let database = PostgreSQLDatabase(config: databaseConfig)
-    databases.add(database: database, as: .psql)
-    services.register(databases)
-
-    // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Acronym.self, database: .psql)
-    services.register(migrations)
-
-    // Configure the rest of your application here
+  // Configure the rest of your application here
+  var commandConfig = CommandConfig.default()
+  commandConfig.useFluentCommands()
+  services.register(commandConfig)
 }
