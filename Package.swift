0a8c15e630fcbacf417b01beb707da72d569416a
Denis Baraley <dbaraley@netpulse.com>
2019-01-31 16:02:19 +0200

    did add leaf to package desc

--- Package.swift
+++ Package.swift
@@ -1,15 +1,21 @@
 // swift-tools-version:4.0
 import PackageDescription
-
 let package = Package(
-  name: "TILApp",
-  dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-    .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-  ],
-  targets: [
-    .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
-    .target(name: "Run", dependencies: ["App"]),
-    .testTarget(name: "AppTests", dependencies: ["App"]),
-  ]
+    name: "TILApp",
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git",
+                 from: "3.0.0"),
+        .package(
+            url: "https://github.com/vapor/fluent-postgresql.git",
+            from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/leaf.git",
+        ], targets: [
+        from: "3.0.0-rc")
+        .target(name: "App",
+        dependencies: ["FluentPostgreSQL",
+        "Vapor",
+        "Leaf"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+        ]
 )
